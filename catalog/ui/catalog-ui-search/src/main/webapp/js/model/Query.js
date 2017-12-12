07b9d7ff6cd90c4a0c37692d7eaa61249414607c
Connor Davey <conman.davey@gmail.com>
2017-12-12 16:59:44 -0700

    DDF-3497 Install Apache Ignite in DDF
    This commit adds working installable Ignite features to DDF.
    
    This is a PROTOTYPE. As of this commit, Ignite features are not ready to be added to a proper DDF release or master branch.
    
    DDF should be largely unaffected by this initial change as no features currently utilize Ignite and Ignite features are set up for manual installation, not automatically installed on startup.

--- catalog/ui/catalog-ui-search/src/main/webapp/js/model/Query.js
+++ catalog/ui/catalog-ui-search/src/main/webapp/js/model/Query.js
@@ -17,6 +17,7 @@ define([
         'properties',
         'js/cql',
         'js/model/QueryResponse',
+        'js/model/QuerySchedule',
         'component/singletons/sources-instance',
         'js/Common',
         'js/CacheSourceSelector',
@@ -26,7 +27,7 @@ define([
         'lodash/merge',
         'backboneassociations',
     ],
-    function (Backbone, _, properties, cql, QueryResponse, Sources, Common, CacheSourceSelector, announcement,
+    function (Backbone, _, properties, cql, QueryResponse, QuerySchedule, Sources, Common, CacheSourceSelector, announcement,
         CQLUtils, user, _merge) {
         "use strict";
         var Query = {};
@@ -65,6 +66,10 @@ define([
                 key: 'result',
                 relatedModel: QueryResponse,
                 isTransient: true
+            }, {
+                type: Backbone.Many,
+                key: 'schedules',
+                relatedModel: QuerySchedule
             }],
             //in the search we are checking for whether or not the model
             //only contains 5 items to know if we can search or not
@@ -84,7 +89,11 @@ define([
                     serverPageIndex: 0,
                     type: 'text',
                     isLocal: false,
+<<<<<<< HEAD
                     isOutdated: false
+=======
+                    schedules: []
+>>>>>>> DDF-3497 Install Apache Ignite in DDF
                 }, user.getQuerySettings().toJSON());
             },
             resetToDefaults: function() {
@@ -106,7 +115,11 @@ define([
                 _.bindAll.apply(_, [this].concat(_.functions(this))); // underscore bindAll does not take array arg
                 this.set('id', this.getId());
                 this.listenTo(user.get('user>preferences'), 'change:resultCount', this.handleChangeResultCount);
+<<<<<<< HEAD
                 this.listenTo(this, 'change:cql', () => this.set('isOutdated', true));
+=======
+                this.listenTo(this.get('schedules'), 'change add remove update', this.handleSchedulesChange);
+>>>>>>> DDF-3497 Install Apache Ignite in DDF
             },
             buildSearchData: function () {
                 var data = this.toJSON();
@@ -347,6 +360,9 @@ define([
                 if (this.get('result')) {
                     this.get('result').resetResultCountsBySource();
                 }
+            },
+            handleSchedulesChange: function() {
+                this.trigger('update');
             },
             getResultsRangeLabel: function (resultsCollection) {
                 var results = resultsCollection.fullCollection.length;
