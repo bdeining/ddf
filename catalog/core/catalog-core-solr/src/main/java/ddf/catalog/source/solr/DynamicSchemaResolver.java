94a1af208836ae2d77de2eb86508f0a45f8424f4
Joseph North <joernorth@gmail.com>
2018-10-29 14:51:37 -0700

    Revert "DDF-3938 Use docValues to improve sorting performance"
    This reverts commit 21545fa2f7d70fbe82fc31710ec65771f6599535.

--- catalog/core/catalog-core-solr/src/main/java/ddf/catalog/source/solr/DynamicSchemaResolver.java
+++ catalog/core/catalog-core-solr/src/main/java/ddf/catalog/source/solr/DynamicSchemaResolver.java
@@ -326,13 +326,8 @@ public class DynamicSchemaResolver {
               solrInputDocument.addField(
                   formatIndexName + SchemaFields.SORT_KEY_SUFFIX,
                   createCenterPoint(attributeValues));
-            } else if (AttributeFormat.STRING.equals(format)) {
-              solrInputDocument.addField(
-                  formatIndexName + SchemaFields.SORT_KEY_SUFFIX,
-                  formatSortKey((String) attributeValues.get(0)));
             } else if (!(AttributeFormat.BINARY.equals(format)
-                || AttributeFormat.OBJECT.equals(format)
-                || AttributeFormat.XML.equals(format))) {
+                || AttributeFormat.OBJECT.equals(format))) {
               solrInputDocument.addField(
                   formatIndexName + SchemaFields.SORT_KEY_SUFFIX, attributeValues.get(0));
             }
@@ -382,10 +377,6 @@ public class DynamicSchemaResolver {
     }
 
     solrInputDocument.addField(SchemaFields.METACARD_TYPE_OBJECT_FIELD_NAME, metacardTypeBytes);
-  }
-
-  private String formatSortKey(String term) {
-    return StringUtils.substring(term.trim(), 0, 127).toLowerCase();
   }
 
   /*
